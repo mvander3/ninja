acffeffc73133e30ab369be4d1ee3e29ea1e67de
Mike Vander Pluym <mvander3@gmail.com>
2010-09-12 18:46:58 -0500

    Name Change

--- src/ninja/lesson/hello/HelloWorld.java
+++ src/ninja/lesson/hello/HelloWorld.java
@@ -3,6 +3,6 @@ package ninja.lesson.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); 
 	}
 }
